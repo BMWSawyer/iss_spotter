35d8557a928337b92b91a1e29b12cd7af034e26a
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-08-23 08:27:21 +0000

    Made major changes to this file by moving most of the code to the iss.js file for it's final function. Re-worked this file to only require one function from the other file and inplemented that function to print out the flyover times for my location

--- index.js
+++ index.js
@@ -1,36 +1,19 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
-fetchMyIP((error, ip) => {
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const dateTime = new Date(0);
+    dateTime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`The next pass is at ${dateTime} for ${duration} seconds!`);
+  }
+};
+
+
+nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
-    console.log('It didn\'t work!', error);
-    return;
+    return console.log("It didn't work!", error);
   }
-
-  fetchCoordsByIP(ip, (err2, data) => {
-    if (err2) {
-      console.log('It didn\'t work!', err2);
-      return;
-    }
   
-    fetchISSFlyOverTimes(data, (err3, data) => {
-      if (err3) {
-        console.log('It didn\'t work!', err3);
-        return;
-      }
-
-      console.log(data);
-    });
-  });
+  printPassTimes(passTimes);
 });
-
-
-/*
-fetchCoordsByIP(fetchMyIP(), (err2, data) => {
-  if (err2) {
-    console.log('It didn\'t work!', err2);
-    return;
-  }
-
-  console.log(data);
-});
-*/
