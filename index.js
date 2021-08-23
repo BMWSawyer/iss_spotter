3dcb60e5f8403834dda862850b956152fac1aa06
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-08-23 05:30:26 +0000

    Added in the test case to make sure that the latitude and longitude coordinates were being received as needed

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
     console.log('It didn\'t work!', error);
     return;
   }
+
+  fetchCoordsByIP(ip, (err2, data) => {
+    if (err2) {
+      console.log('It didn\'t work!', err2);
+      return;
+    }
   
-  console.log('It worked! Returned IP: ', ip);
+    console.log('It worked! Returned coordinates: ', data);
+  });
 });
+
+
+/*
+fetchCoordsByIP(fetchMyIP(), (err2, data) => {
+  if (err2) {
+    console.log('It didn\'t work!', err2);
+    return;
+  }
+
+  console.log(data);
+});
+*/
