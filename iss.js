e7e4120dd483bcaccf8f37efd072c25f660da80c
BMWSawyer <bradley.mw.sawyer@gmail.com>
2021-08-23 08:04:31 +0000

    Added the functionality to be able to retrieve the flyover times of the ISS. Had to use the toggled answer as the webpage Compass links to says the API has been removed

--- iss.js
+++ iss.js
@@ -41,8 +41,33 @@ const fetchCoordsByIP = function(ip, callback) {
   });
 };
 
+const fetchISSFlyOverTimes = function(coords, callback) {
+  request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`, (err3, response, body) => {
+    
+    if (err3) {
+      callback(err3, null);
+      return;
+    }
+    
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching the ISS flyover times. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    
+    const passes = JSON.parse(body).response;
+    
+    callback(null, passes);
+
+  });
+}
+
+const nextISSTimesForMyLocation = function(callback) {
+  // empty for now
+};
 
 module.exports = {
   fetchMyIP,
-  fetchCoordsByIP
+  fetchCoordsByIP,
+  fetchISSFlyOverTimes
 };
